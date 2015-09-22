98957107cc99cda75e2a4cd7b4e49a5ca19714ba
socialkick <socialkick@users.noreply.github.com>
2015-09-22 16:17:08 -0700

    06-New Header

--- header.php
+++ header.php
@@ -1,17 +1,25 @@
 <div class="linkAnchor" id="top"></div>
 
 <header>
-    <div id="headerInner">
-        <a href="#top">
-            <img id="logo" src="img/logo.png" alt="Meela Shah Indian Cooking">
+    <div id="headerInner" class="clearfix">
+        <a href="#top" id="logo" class="borderBox">
+            <!-- <img src="img/logo.png" alt="Meela Shah Indian Cooking"> -->
+            <h1>
+                Meela Shah<span>Indian Cooking</span>
+            </h1>
         </a>
         <div id="menuToggle"></div>
         <nav>
             <ul>
-                <li><a href="#ClassesAndEvents" onclick="toggle('headerInner')">Classes & Events</a></li>
-                <li><a href="#MasalaDabba" onclick="toggle('headerInner')">Spice Boxes</a></li>
-                <li><a href="#Recipes" onclick="toggle('headerInner')">Recipes</a></li>
-                <li><a href="#Contact" onclick="toggle('headerInner')">Contact</a></li>
+                <li>
+                    <a href="#ClassesAndEvents" onclick="toggle('headerInner')">Classes & Events</a>
+                </li><li>
+                    <a href="#MasalaDabba" onclick="toggle('headerInner')">Spice Boxes</a>
+                </li><li>
+                    <a href="#Recipes" onclick="toggle('headerInner')">Recipes</a>
+                </li><li>
+                    <a href="#Contact" onclick="toggle('headerInner')">Contact</a>
+                </li>
             </ul>
         </nav>
     </div>
